80ef12ad0cc6031172ccd55a68b43c27950ce687
Mathew B <bogaert@email.arizona.edu>
2021-02-28 16:48:19 -0700

    footer and header components started, with some css tweaks

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            Social Media links will go here
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="flex-row px-1">
+            <h1>Mathew Bogaert</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <div>About Me</div>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <div>Portfolio</div>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <div>Contact</div>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <div>Resume</div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
