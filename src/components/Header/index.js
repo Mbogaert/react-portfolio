e4d8f066d13f6e560020ef44ca6f7ff005727216
Mathew B <bogaert@email.arizona.edu>
2021-02-28 19:07:48 -0700

    finished contact form besides styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,16 +8,16 @@ function Header(props) {
             <h1>Mathew Bogaert</h1>
             <nav>
                 <ul>
-                    <li class={currentPage === "About"}>
+                    <li className={currentPage === "About"}>
                         <span onClick={() => handlePageChange("About")}>About Me</span>
                     </li>
-                    <li class={currentPage === "Portfolio"}>
+                    <li className={currentPage === "Portfolio"}>
                         <span onClick={() => handlePageChange("Portfolio")}>Portfolio</span>
                     </li>
-                    <li class={currentPage === "Contact"}>
+                    <li className={currentPage === "Contact"}>
                         <span onClick={() => handlePageChange("Contact")}>Contact Me</span>
                     </li>
-                    <li class={currentPage === "Resume"}>
+                    <li className={currentPage === "Resume"}>
                         <span onClick={() => handlePageChange("Resume")}>Resume</span>
                     </li>
                 </ul>
