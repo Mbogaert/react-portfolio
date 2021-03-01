8fb940d7707ec2cfdcb96f725fcd375426fb637e
Mathew B <bogaert@email.arizona.edu>
2021-02-28 19:45:51 -0700

    links added to footer, styling still needed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import etsy from '../../assets/icons/etsy-icon.png';
+import gitHub from '../../assets/icons/github-icon.png';
+import linkedIn from '../../assets/icons/linkedin-icon.png';
 
 function Footer() {
     return (
         <footer>
-            Social Media links will go here
+            <a href="https://github.com/Mbogaert"><img src={gitHub} style={{ width: "10% "}} alt=""/></a>
+            <a href="https://www.linkedin.com/in/mathewbogaert/"><img src={linkedIn} style={{ width: "10% "}} alt=""/></a>
+            <a href="https://www.etsy.com/shop/pokements/"><img src={etsy} style={{ width: "10% "}} alt=""/></a>
         </footer>
     )
 }
