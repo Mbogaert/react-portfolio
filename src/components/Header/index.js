9fc033c5c1c41d644e9b69c321d37a2b1d23d199
Mathew B <bogaert@email.arizona.edu>
2021-02-28 20:05:21 -0700

    highlighted in nav bar when on a certain page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,16 +8,16 @@ function Header(props) {
             <h1>Mathew Bogaert</h1>
             <nav>
                 <ul>
-                    <li className={currentPage === "About"}>
+                    <li className={currentPage === "About" ? "nav-link navActive" : "nav-link"}>
                         <span onClick={() => handlePageChange("About")}>About Me</span>
                     </li>
-                    <li className={currentPage === "Portfolio"}>
+                    <li className={currentPage === "Portfolio" ? "nav-link navActive" : "nav-link"}>
                         <span onClick={() => handlePageChange("Portfolio")}>Portfolio</span>
                     </li>
-                    <li className={currentPage === "Contact"}>
+                    <li className={currentPage === "Contact" ? "nav-link navActive" : "nav-link"}>
                         <span onClick={() => handlePageChange("Contact")}>Contact Me</span>
                     </li>
-                    <li className={currentPage === "Resume"}>
+                    <li className={currentPage === "Resume" ? "nav-link navActive" : "nav-link"}>
                         <span onClick={() => handlePageChange("Resume")}>Resume</span>
                     </li>
                 </ul>
