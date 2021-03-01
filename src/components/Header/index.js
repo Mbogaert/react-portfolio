636f65d513057a516e3aa75c6eedfe96ef1a3782
Mathew B <bogaert@email.arizona.edu>
2021-02-28 20:12:56 -0700

    navigation styling tweaked

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,20 +4,20 @@ function Header(props) {
     const { currentPage, handlePageChange } = props;
 
     return (
-        <header>
+        <header className="flex-row">
             <h1>Mathew Bogaert</h1>
             <nav>
-                <ul>
-                    <li className={currentPage === "About" ? "nav-link navActive" : "nav-link"}>
+                <ul className="flex-row">
+                    <li className={currentPage === "About" ? "nav-link navActive" : "nav-link mx-2"}>
                         <span onClick={() => handlePageChange("About")}>About Me</span>
                     </li>
-                    <li className={currentPage === "Portfolio" ? "nav-link navActive" : "nav-link"}>
+                    <li className={currentPage === "Portfolio" ? "nav-link navActive" : "nav-link mx-2"}>
                         <span onClick={() => handlePageChange("Portfolio")}>Portfolio</span>
                     </li>
-                    <li className={currentPage === "Contact" ? "nav-link navActive" : "nav-link"}>
+                    <li className={currentPage === "Contact" ? "nav-link navActive" : "nav-link mx-2"}>
                         <span onClick={() => handlePageChange("Contact")}>Contact Me</span>
                     </li>
-                    <li className={currentPage === "Resume" ? "nav-link navActive" : "nav-link"}>
+                    <li className={currentPage === "Resume" ? "nav-link navActive" : "nav-link mx-2"}>
                         <span onClick={() => handlePageChange("Resume")}>Resume</span>
                     </li>
                 </ul>
